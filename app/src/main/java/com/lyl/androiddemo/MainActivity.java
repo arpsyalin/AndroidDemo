12444dad94fda40f5508f16d7f419b3186a1dddb
lyl <461973266@qq.com>
2020-11-20 23:34:47 +0800

    添加例子

---
+++ app/src/main/java/com/lyl/androiddemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lyl.androiddemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new GradientLayout(this));
+    }
+}
